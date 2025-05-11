ddceeb281df09998940c9037c9c627cae73fe875
Nadia Batchelor <nadabdesigns@gmail.com>
2025-05-11 16:37:59 -0400

    edits

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-let hover = addEventListener
+let manuscript = document.getElementById('01')
 
-
-
-
-
-function change() {
-	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/01')";
-  }
-
-function set01(){
-	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/01')";
-
-}
-
-function set02(){
-	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/02')";
-
-}
-
-function set03(){
-	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/03')";
-
-}
-
-function set04(){
-	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/04')";
-
-}
-function set05(){
-	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/05')";
-
-}
-
+manuscript.
+ 
