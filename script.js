f4acb5ef0e4693698b22d02c336d71fb68c3950c
Nadia Batchelor <nadabdesigns@gmail.com>
2025-05-11 17:43:25 -0400

    javascript

--- script.js
+++ script.js
@@ -1,22 +1,93 @@
 let manuscript = document.getElementById('01')
+let spread = document.getElementById('02')
+let binding = document.getElementById('03')
+let links = document.getElementById('04')
+let functions = document.getElementById('05')
+
 let background = document.getElementsByTagName('body')[0]
 let text = document.getElementById('01text')
 
-manuscript.addEventListener('mouseover',(e)=>{
+
+manuscript.addEventListener('mouseover', (e) => {
 	background.style.backgroundImage = "url('assets/desktop/01.png')";
 	console.log(background)
 	text.textContent = '01';
 	text.style.color = 'black';
+	e.target.style.background = 'black';
+	e.target.style.color = 'white';
 
-	
-
-	e.target.style.color ='purple'
-	
 })
-manuscript.addEventListener('mouseout',(e)=>{
+manuscript.addEventListener('mouseout', (e) => {
+	// background image
 	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
-	e.target.style.color ='black'
+	// big text color
+	e.target.style.color = 'black'
+	// small text syle
+	e.target.style.background = '';
+	
+	text.style.color = 'white';
+	text.textContent = '00'
+})
+spread.addEventListener('mouseover', (e) => {
+	background.style.backgroundImage = "url('assets/desktop/01.png')";
+	console.log(background)
+	text.textContent = '02';
+	text.style.color = 'black';
+	e.target.style.color = 'purple';
+
+})
+spread.addEventListener('mouseout', (e) => {
+	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
+	e.target.style.color = 'black'
+	text.style.color = 'white';
+	text.textContent = '00'
+})
+binding.addEventListener('mouseover', (e) => {
+	background.style.backgroundImage = "url('assets/desktop/01.png')";
+	console.log(background)
+	text.textContent = '03';
+	text.style.color = 'black';
+	e.target.style.color = 'purple';
+
+})
+binding.addEventListener('mouseout', (e) => {
+	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
+	e.target.style.color = 'black'
+	text.style.color = 'white';
+	text.textContent = '00'
+})
+links.addEventListener('mouseover', (e) => {
+	background.style.backgroundImage = "url('assets/desktop/01.png')";
+	console.log(background)
+	text.textContent = '04';
+	text.style.color = 'black';
+	e.target.style.color = 'purple';
+
+})
+links.addEventListener('mouseout', (e) => {
+	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
+	e.target.style.color = 'black'
+	text.style.color = 'white';
+	text.textContent = '00'
+})
+functions.addEventListener('mouseover', (e) => {
+	background.style.backgroundImage = "url('assets/desktop/01.png')";
+	console.log(background)
+	text.textContent = '05';
+	text.style.color = 'black';
+	e.target.style.color = 'purple';
+
+})
+functions.addEventListener('mouseout', (e) => {
+	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
+	e.target.style.color = 'black'
+	text.style.color = 'white';
 	text.textContent = '00'
 })
 
- 
+
+
+
+
+
+
