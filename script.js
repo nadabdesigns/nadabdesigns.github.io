259852469df88534f412db9608066c9d9dc01b21
Nadia Batchelor <nadabdesigns@gmail.com>
2025-05-10 23:55:00 -0400

    testing fuctions

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-addEventListener("mouseover", (event) => { })
+function change() {
+	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/01')";
+  }
 
-onmouseover = (event) => { }
+function set01(){
+	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/01')";
+
+}
+
+function set02(){
+	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/02')";
+
+}
+
+function set03(){
+	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/03')";
+
+}
+
+function set04(){
+	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/04')";
+
+}
+function set05(){
+	document.getElementById("mobile").style.backgroundImage = "url('/assets/mobile/05')";
+
+}
+
