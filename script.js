97bf2e69877c117fde2d0ea890c2de76e5204247
Nadia Batchelor <nadabdesigns@gmail.com>
2025-05-11 17:20:36 -0400

    changes

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let manuscript = document.getElementById('01')
+let background = document.getElementsByTagName('body')[0]
+let text = document.getElementById('01text')
 
 manuscript.addEventListener('mouseover',(e)=>{
+	background.style.backgroundImage = "url('assets/desktop/01.png')";
+	console.log(background)
+	text.textContent = '01';
+	text.style.color = 'black';
+
+	
 
 	e.target.style.color ='purple'
+	
 })
 manuscript.addEventListener('mouseout',(e)=>{
-
+	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
 	e.target.style.color ='black'
+	text.textContent = '00'
 })
+
  
