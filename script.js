ec554e85fa84cf304b7794e6461d9ac2742cc2f7
Nadia Batchelor <nadabdesigns@gmail.com>
2025-05-11 22:14:43 -0400

    almost done

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ manuscript.addEventListener('mouseover', (e) => {
 })
 manuscript.addEventListener('mouseout', (e) => {
 	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
-	
+
 	// e.target.style.color = 'black'
 	text.style.color = 'white';
-	
+
 	text.textContent = '00'
 	// svgbk.stylebackgroundImage = "url('assets/cow.svg')";
 })
@@ -39,12 +39,12 @@ spread.addEventListener('mouseover', (e) => {
 	text.textContent = '02';
 	text.style.color = 'black';
 	// e.target.style.color = 'purple';
-	
+
 
 })
 spread.addEventListener('mouseout', (e) => {
 	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
-	
+
 	// e.target.style.color = 'black'
 	text.style.color = 'white';
 	text.textContent = '00'
@@ -105,13 +105,52 @@ functions.addEventListener('mouseout', (e) => {
 	background.style.backgroundImage = "url('assets/IMAG0031.jpg')";
 	// e.target.style.color = 'black'
 	text.style.color = 'white';
-	
+
 	text.textContent = '00'
 	text.style.mixBlendMode = 'normal';
 })
 
 
 
+
+
+// For MOBILE
+
+// store data for images
+let box = document.getElementById('mobile');
+let images = [
+  'url("assets/cow_mobile")',
+  'url("assets/mobile/01.jpg")',
+  'url("assets/mobile/02.jpg")',
+  'url("assets/mobile/03.jpg")',
+  'url("assets/mobile/04.jpg")',
+  'url("assets/mobile/05.jpg")'
+];
+let index = 0;
+// grab data and replace
+setInterval(() => {
+  box.style.backgroundImage = images[index];
+  index = (index + 1) % images.length;
+}, 3000); 
+
+// change the number value 
+let textElement = document.getElementById('numb'); 
+let count = 0;
+
+// count up from 0
+setInterval(() => {
+  let padded = count.toString().padStart(2, '0');
+  textElement.textContent = padded;
+
+//   if the number value is 04 change the color to black
+  if (padded === '04') {
+    textElement.style.color = 'black';
+  } else {
+    textElement.style.color = 'white';
+  }
+
+  count = (count + 1) % 6;
+}, 3000);
 
 
 
